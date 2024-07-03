b441992ec045ca55d3bbbae443e093f0eb2ca67d
warcjh <warcjh00@naver.com>
2024-07-03 17:00:37 +0900

    0703 함수 문제풀이

---
+++ src/method/quiz/Quiz7.java
@@ -0,0 +1,21 @@
+package method.quiz;
+
+public class Quiz7 {
+
+	// 7번. 세자리 정수의 각 자리수의 합을 구하는 메소드를 만들고 호출하세요.
+	public static void num(int n1) {
+		int sum = 0;
+		sum = n1/100;
+		
+		
+		
+		System.out.println(sum);
+		
+	}
+	
+	public static void main(String[] args) {
+		num(932);
+
+	}
+
+}
